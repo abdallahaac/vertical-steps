885314f9cd7c1b0124a1ba8e1961b06699fe7210
Abdallah Abou-Chahine  (CSPS-EFPC) <abdallah.abou-chahine@csps-efpc.gc.ca>
2023-05-12 15:20:35 -0400

    inital commit

--- vertical-steps.js
+++ vertical-steps.js
@@ -1,32 +1,104 @@
+const PROGRESS_BAR_SELECTOR = ".loader";
+const SCROLL_MULTIPLIER = 20;
+const MAX_HEIGHT = 200;
+
+const progressBars = Array.from(
+	document.querySelectorAll(PROGRESS_BAR_SELECTOR)
+);
+const [progressBar1, progressBar2] = progressBars;
+
 let scrollAmount = 0;
-let progressBars = document.querySelectorAll(".loader");
 let currentHeight = 0;
-let isProgressBarLoading = true;
+let isSecondProgressBarVisible = false;
 
-window.addEventListener("wheel", (event) => {
+function clampHeight(value, min, max) {
+	return Math.max(min, Math.min(max, value));
+}
+
+function showProgressBar(progressBar) {
+	progressBar.style.display = "block";
+}
+
+function setProgressBarHeight(progressBar, height) {
+	progressBar.style.height = `${height}px`;
+}
+
+function updateProgressBar(progressBar, height) {
+	showProgressBar(progressBar);
+	setProgressBarHeight(progressBar, height);
+}
+
+function showStep2Icon() {
+	const step2Icon = document.querySelector('[data-step="2"]');
+	step2Icon.classList.remove("disabled");
+}
+function hideStep2Icon() {
+	const step2Icon = document.querySelector('[data-step="2"]');
+	step2Icon.classList.add("disabled");
+}
+
+function hideStep3Icon() {
+	const step3Icon = document.querySelector('[data-step="3"]');
+	step3Icon.classList.add("disabled");
+}
+function showStep3Icon() {
+	const step3Icon = document.querySelector('[data-step="3"]');
+	step3Icon.classList.remove("disabled");
+}
+
+function updateScrollAmount(event) {
 	const delta = Math.sign(event.deltaY);
-	scrollAmount += delta * 5; // Increase speed by multiplying with a factor
-	console.log(scrollAmount);
-
-	if (isProgressBarLoading && currentHeight >= 0 && currentHeight <= 170) {
-		event.preventDefault();
-		progressBars[0].style.height = `${currentHeight + scrollAmount}px`; // Set height to current height + scroll amount
-		currentHeight += scrollAmount;
-		if (currentHeight < 0) {
-			currentHeight = 0;
-			scrollAmount = 0;
-		} else if (currentHeight > 150) {
-			currentHeight = 150;
-			scrollAmount = 0;
-			isProgressBarLoading = false;
-		}
+	scrollAmount += delta * SCROLL_MULTIPLIER;
+	if (delta < 0) {
+		scrollAmount = 1;
 	}
+}
 
-	if (!isProgressBarLoading) {
-		// Allow scrolling after the progress bar has finished loading
-		document.body.style.overflowY = "scroll";
+function updateCurrentHeight() {
+	currentHeight += scrollAmount;
+	currentHeight = clampHeight(currentHeight, 1, MAX_HEIGHT);
+}
+
+function updateFirstProgressBar() {
+	updateProgressBar(progressBar1, currentHeight);
+	scrollAmount = 1;
+	if (currentHeight >= MAX_HEIGHT && !isSecondProgressBarVisible) {
+		console.log("Maximum length reached for the first progress bar");
+		isSecondProgressBarVisible = true;
+		showProgressBar(progressBar2);
+		showStep2Icon();
+		scrollAmount = 0;
+	}
+}
+
+function updateSecondProgressBar() {
+	updateProgressBar(progressBar2, 300);
+}
+
+function handleFirstProgressBar() {
+	if (currentHeight <= MAX_HEIGHT) {
+		updateCurrentHeight();
+		updateFirstProgressBar();
+	} else if (scrollAmount > 0) {
+		updateSecondProgressBar();
 	} else {
-		// Prevent scrolling while the progress bar is loading
-		document.body.style.overflowY = "hidden";
+		updateProgressBar(progressBar2, currentHeight);
 	}
-});
+}
+
+function handleStep2Icon() {
+	if (scrollAmount != 1) {
+		hideStep2Icon();
+	} else {
+		showStep2Icon();
+	}
+}
+
+function handleScroll(event) {
+	updateScrollAmount(event);
+	handleFirstProgressBar();
+	handleStep2Icon();
+	console.log(scrollAmount);
+}
+
+window.addEventListener("wheel", handleScroll);
